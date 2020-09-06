c0e7e7501aed6efe11ef8dbc52678f08a99f602d
Epitech <epitech@sd-161731.dedibox.fr>
2020-09-06 13:26:14 +0200

    ok

--- module.tf
+++ module.tf
@@ -3,20 +3,20 @@ module "myazure_westeurope" {
   source = "./myazure"
   zone_key = "westeurope"
   zone_label = "West Europe"
-  serverid_prefix = 1
+  serverid_prefix = 2
 }
 
 module "myazure_northeurope" {
   source = "./myazure"
   zone_key = "northeurope"
   zone_label = "North Europe"
-  serverid_prefix = 2
+  serverid_prefix = 3
 }
 
 module "myazure_francecentral" {
   source = "./myazure"
   zone_key = "francecentral"
   zone_label = "France Central"
-  serverid_prefix = 3
+  serverid_prefix = 4
 }
 
